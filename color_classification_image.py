90bc26f6b30643debced85c7c513e4b1173c076f
han <chauthehan00@gmail.com>
2021-04-15 20:13:35 +0700

    ver1.0

---
+++ color_classification_image.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# -------------------------------------------------------------------------
+# --- Author         : Ahmet Ozlu
+# --- Mail           : ahmetozlu93@gmail.com
+# --- Date           : 8th July 2018 - before Google inside look 2018 :)
+# -------------------------------------------------------------------------
+
+import cv2
+from color_recognition_api import color_histogram_feature_extraction
+from color_recognition_api import knn_classifier
+import os
+import os.path
+import sys
+from imutils import paths
+import csv
+import random
+import math
+import operator
+
+def calculateEuclideanDistance(variable1, variable2, length):
+    distance = 0
+    for x in range(length):
+        distance += pow(variable1[x] - variable2[x], 2)
+    return math.sqrt(distance)
+
+
+def kNearestNeighbors(training_feature_vector, testInstance, k):
+    distances = []
+    length = len(testInstance)
+    for x in range(len(training_feature_vector)):
+        dist = calculateEuclideanDistance(testInstance,
+                training_feature_vector[x], length)
+        distances.append((training_feature_vector[x], dist))
+    distances.sort(key=operator.itemgetter(1))
+    neighbors = []
+    for x in range(k):
+        neighbors.append(distances[x][0])
+    return neighbors
+
+def responseOfNeighbors(neighbors):
+    all_possible_neighbors = {}
+    for x in range(len(neighbors)):
+        response = neighbors[x][-1]
+        if response in all_possible_neighbors:
+            all_possible_neighbors[response] += 1
+        else:
+            all_possible_neighbors[response] = 1
+    sortedVotes = sorted(all_possible_neighbors.items(),
+                         key=operator.itemgetter(1), reverse=True)
+    return sortedVotes[0][0]
+
+
+# read the test image
+# checking whether the training data is ready
+PATH = './training.data'
+
+if os.path.isfile(PATH) and os.access(PATH, os.R_OK):
+    print ('training data is ready, classifier is loading...')
+else:
+    print ('training data is being created...')
+    open('training.data', 'w')
+    color_histogram_feature_extraction.training()
+    print ('training data is ready, classifier is loading...')
+
+
+training_feature_vector = []  # training feature vector
+with open('training.data') as csvfile:
+    lines = csv.reader(csvfile)
+    dataset = list(lines)
+    for x in range(len(dataset)):
+        for y in range(3):
+            dataset[x][y] = float(dataset[x][y])
+        training_feature_vector.append(dataset[x])
+
+while True:
+    for i in paths.list_images('/home/han/Documents/ocr/result/white'):
+        img = cv2.imread(i)
+        test_feature_vector = []  # test feature vector
+    # get the prediction
+        color_histogram_feature_extraction.color_histogram_of_test_image(img)
+
+        with open('test.data') as csvfile:
+            lines = csv.reader(csvfile)
+            dataset = list(lines)
+            for x in range(len(dataset)):
+                for y in range(3):
+                    dataset[x][y] = float(dataset[x][y])
+                test_feature_vector.append(dataset[x])
+        classifier_prediction = []
+        k = 4
+        for x in range(len(test_feature_vector)):
+            neighbors = kNearestNeighbors(training_feature_vector, test_feature_vector[x], k)
+            result = responseOfNeighbors(neighbors)
+            classifier_prediction.append(result)
+
+        print('Detected color is:', classifier_prediction[0])
